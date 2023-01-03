2c006321eae83df4d483c2caad15b771187f95a6
julianaflima <juli.faccio@gmail.com>
2023-01-03 08:07:00 +0530

    add UI

--- javascript.js
+++ javascript.js
@@ -1,82 +1,68 @@
-// Get a random number between 0 and 2
-function getRandom012() {
-	return Math.floor(Math.random() * 3);
-}
-
-
-// Translate random number to computer play (Rock/Paper/Scissor)
-function getComputerChoice() {
-	let randomNumber = getRandom012();
-	let computerSelection;
-
-	// Assign choice to number
-	if (randomNumber === 0) {
-		return computerSelection = "Rock";
-	} else if (randomNumber === 1) {
-		return computerSelection = "Paper";
-	} else {
-		return computerSelection = "Scissor";
-	}
-}
-
-
-
-// Capitalize first letter 
-function capitalizeFirstLetter(string) {
-	return string.slice(0,1).toUpperCase() + string.slice(1).toLowerCase()
-}
-
-
-
-
-
-let message;
+// Set score to 0 when page is refreshed
 let computerScore = playerScore = 0;
+let counter = 1;
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
 
-// Compare user play with computer play
-function playRound(playerSelection, computerSelection) {
-	if (playerSelection === computerSelection) {
-		return message = `It's a tie!`;
+// Get a random number and translate into a play
+function getComputerChoice() {
+	let randomNumber = Math.floor(Math.random() * 3);
+	switch (randomNumber) {
+		case 0: return "Rock";
+		case 1: return "Paper";
+		case 2: return "Scissor";
 	}
-	else if (playerSelection === 'Rock' && computerSelection === 'Scissor' || playerSelection === 'Scissor' && computerSelection === 'Paper' || playerSelection === 'Paper' && computerSelection === 'Rock') {
+}
+
+// Disable button
+function disableButton(){
+	buttons.forEach(button =>
+		button.removeEventListener('click', playRound));
+}
+
+// Get winner of each round
+function playRound(e) {
+	let playerSelection = e.target.textContent;
+	let computerSelection = getComputerChoice();
+	let roundMessage;
+
+	if (playerSelection === computerSelection) {
+		roundMessage = `It's a tie!`;
+	}	else if (playerSelection === 'Rock' && computerSelection === 'Scissor' || playerSelection === 'Scissor' && computerSelection === 'Paper' || playerSelection === 'Paper' && computerSelection === 'Rock') {
 		playerScore += 1;
-		return message = `You win! ${playerSelection} beats ${computerSelection}`;
+		roundMessage = `You win! ${playerSelection} beats ${computerSelection}`;
 	} else {
 		computerScore += 1;
-		return message = `You lose! ${computerSelection} beats ${playerSelection}`;
+		roundMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
 	}
-	
+
+	const roundResult = document.createElement('div');
+	roundResult.textContent = `Round ${counter}: ${roundMessage}`;
+	body.appendChild(roundResult);
+	++counter;
+
+	// End game when it gets to round 5
+	if (counter === 6) endGame();
 }
 
+function endGame() {
+		disableButton();
+		let finalMessage = '';
 
+		if (computerScore > playerScore) {
+			finalMessage = `You lost! ${computerScore} vs ${playerScore}`
+		}	else if (playerScore > computerScore) {
+			finalMessage = `You won! ${playerScore} vs ${computerScore}`;
+		} else {
+			finalMessage = `It's a tie! ${computerScore} vs ${playerScore}`;
+		}
 
-
-// Allow 5 rounds and announce the winner
-function game() {
-	for(let i = 1; i < 6; i++) {
-
-		// Ask for user to choose an option
-		let playerSelection = capitalizeFirstLetter(prompt(`Round ${i}: Rock, Paper, or Scissor?`));
-
-		// Randomly generate computer play
-		let computerSelection = getComputerChoice();
-
-		// Get the winner of the round
-		console.log(playRound(playerSelection, computerSelection));
-	}
-
-	// Show final score count
-	console.log(`Player score: ${playerScore}`);
-	console.log(`Computer score: ${computerScore}`);
-
-	//Get the winner of 5 rounds
-	if (computerScore > playerScore) {
-		console.log(`You lost! ${computerScore} vs ${playerScore}`);
-	}	else if (playerScore > computerScore) {
-		console.log(`You won! ${playerScore} vs ${computerScore}`);
-	} else {
-		console.log(`It's a tie! ${computerScore} vs ${playerScore}`);
-	}
+		const finalResult = document.createElement('div')
+		finalResult.innerHTML = finalMessage + '<br> Reload the page to play again.';
+		body.insertBefore(finalResult, body.lastElementChild.nextSibling);
+	// }
 }
 
-game();
+buttons.forEach(button => {
+	button.addEventListener('click', playRound);
+});
